2dd887942dbd2451e73cf9c9cee67a264ca182c0
Perspecto <144237984+Perspecto@users.noreply.github.com>
2023-12-04 11:01:24 +0000

    er

--- Exos_web/AFVM12/script.js
+++ Exos_web/AFVM12/script.js
@@ -1,9 +1,3 @@
-function setup(){
-    createCanvas(500,600);
-    background(237,84,7);
-    translate(width/2,height/2);
-}
-
 function changeColor(color) { 
     document.body.style.background = color ; 
 }
