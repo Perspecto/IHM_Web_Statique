0380cda228d86780d7c41ef998aafd40438fb1f0
Perspecto <144237984+Perspecto@users.noreply.github.com>
2023-12-15 12:46:27 +0000

    ?

---
+++ Pyscript/main.py
@@ -0,0 +1,7 @@
+def rectangle(nbr_longueur,nbr_largeur):
+    print('#'*nbr_longueur)
+    for _ in range(nbr_largeur-2):
+        print(f"#{' '*(nbr_longueur-2)}#")
+    print('#'*nbr_longueur)
+
+
