ce04b3a1611c606c51c76beda7ffae53b989929a
Perspecto <144237984+Perspecto@users.noreply.github.com>
2023-12-18 10:36:44 +0000

    maybe

--- Mon_Pyscript/code.py
+++ Mon_Pyscript/code.py
@@ -1,7 +1,7 @@
 from pyscript import document
 
 
-def translate_english(event):
+def translate(event):
     input_text = document.querySelector("#english")
     english = input_text.value
     output_div = document.querySelector("#output")
