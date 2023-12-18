177d58a66a1c22671822e8478ec5c93bfde6202c
Perspecto <144237984+Perspecto@users.noreply.github.com>
2023-12-18 11:00:30 +0000

    MARCHE !

--- Mon_Pyscript/code.py
+++ Mon_Pyscript/code.py
@@ -1,7 +1,22 @@
 from pyscript import document
 
+def Crypter(texte,decaler):
+    txt=''
+    for lettre in texte:
+        if lettre==' ':
+            txt+=lettre
+        else:
+            lettre=ord(lettre.upper())
+            lettre-=65
+            lettre+=decaler
+            lettre%=26
+            lettre+=65
+            txt+=chr(lettre)
+    return txt
 
 def translate(event):
-    input_text = document.querySelector("#english")
+    input_text = document.querySelector("#texte")
+    decaler = document.querySelector("#decalage")
     english = input_text.value
-    output_div = document.querySelector("#output")
+    output_div = document.querySelector("#sortie")
+    output_div.innerText = Crypter(input_text,decaler)
